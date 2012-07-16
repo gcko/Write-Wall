76e9da069779230302c5e099b5357082b8611fb7
gcko <jscott@coverity.com>
2012-07-16 14:09:07 -0700

    Putting in Google Analytics for Write Wall

--- app/js/background.js
+++ app/js/background.js
@@ -17,5 +17,15 @@
     chrome.storage.sync.MAX_SUSTAINED_WRITE_OPERATIONS_PER_MINUTE = 60; // 1 per second
     chrome.storage.sync.MAX_WRITE_OPERATIONS_PER_HOUR = 3600; // 1 per second for an hour
 
-})(chrome)
+})(chrome);
 
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-33436156-1']);
+_gaq.push(['_trackPageview']);
+
+// Add in Google Analytics
+(function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = 'https://ssl.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
--- app/js/main.js
+++ app/js/main.js
@@ -44,3 +44,13 @@
         }, CHANGE_DELAY));
 
 })(jQuery, chrome);
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-33436156-1']);
+_gaq.push(['_trackPageview']);
+// Add in Google Analytics
+(function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = 'https://ssl.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
