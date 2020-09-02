e71cc9cfa8da5a8cf1db84c8600987e2a85b6b30
Jared Scott <jared.scott@variable.team>
2020-09-02 18:47:21 +0800

    Add in eslint, prettier, and other dev packages
    add in package.json
    
    reformat code according to prettier

--- app/js/background.js
+++ app/js/background.js
@@ -1,16 +1,17 @@
-/* Copyright 2012 Jared M. Scott */
+/* Copyright 2012-2020 Jared M. Scott */
+'use strict';
 /**
- * This work is licensed under the Creative Commons Attribution 3.0 Unported License.
+ * This work is licensed under the Creative Commons Attribution 3.0 Un-ported License.
  *  To view a copy of this license, visit http://creativecommons.org/licenses/by/3.0/
  *  or send a letter to
  *      Creative Commons,
  *      444 Castro Street, Suite 900,
  *      Mountain View, California, 94041, USA.
  */
-(function(chrome){
-    chrome.browserAction.onClicked.addListener(function(tab) {
-        chrome.tabs.create({url:chrome.extension.getURL("html/index.html")});
-    });
+(function (chrome) {
+  chrome.browserAction.onClicked.addListener(function (tab) {
+    chrome.tabs.create({url: chrome.extension.getURL("html/index.html")});
+  });
 
 })(chrome);
 
--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,4 @@
-/* Copyright 2012 Jared M. Scott */
+/* Copyright 2012-2020 Jared M. Scott */
 'use strict';
 /**
  * This work is licensed under the Creative Commons Attribution 3.0 Unported License.
@@ -9,46 +9,47 @@
  *      Mountain View, California, 94041, USA.
  */
 ((chrome) => {
-    let CHANGE_DELAY = (chrome.storage.sync.MAX_WRITE_OPERATIONS_PER_HOUR / 3600 ) * 4000, // 2 second sync delay
-      LEGACY_STORAGE_KEY = 'text',
-      STORAGE_KEY = 'v2',
-      remoteStoredText = '',
-      textAreaEl = document.getElementById('text'),
-      storage = chrome.storage,
-      storageObject = {};
+  let CHANGE_DELAY = (chrome.storage.sync.MAX_WRITE_OPERATIONS_PER_HOUR / 3600) * 4000, // 2 second sync delay
+    LEGACY_STORAGE_KEY = 'text',
+    STORAGE_KEY = 'v2',
+    remoteStoredText = '',
+    textAreaEl = document.getElementById('text'),
+    storage = chrome.storage,
+    storageObject = {};
 
-    const updateUsage = () => {
-        storage.sync.getBytesInUse(null, inUse => document.getElementById('num-chars').innerText = inUse);
-    };
+  const updateUsage = () => {
+    storage.sync.getBytesInUse(null, inUse => document.getElementById('num-chars').innerText = inUse);
+  };
 
-    // Set the number of bytes in use
-    updateUsage();
+  // Set the number of bytes in use
+  updateUsage();
 
-    // get or create key to store data
-    storage.sync.get([LEGACY_STORAGE_KEY, STORAGE_KEY], items => {
-        if (items[LEGACY_STORAGE_KEY] != null) {
-            // Migrate stored data from the previous version to the new version
-            remoteStoredText = items[LEGACY_STORAGE_KEY];
-            storageObject[STORAGE_KEY] = remoteStoredText;
-            storage.sync.set(storageObject);
-            // Remove the legacy key
-            storage.sync.remove(LEGACY_STORAGE_KEY);
-        } else if (items[STORAGE_KEY] != null) {
-            remoteStoredText = items[STORAGE_KEY];
-        }
-        // Value defaults to an empty string if there is no stored value
-        textAreaEl.value = remoteStoredText;
+  // get or create key to store data
+  storage.sync.get([LEGACY_STORAGE_KEY, STORAGE_KEY], items => {
+    if (items[LEGACY_STORAGE_KEY] != null) {
+      // Migrate stored data from the previous version to the new version
+      remoteStoredText = items[LEGACY_STORAGE_KEY];
+      storageObject[STORAGE_KEY] = remoteStoredText;
+      storage.sync.set(storageObject);
+      // Remove the legacy key
+      storage.sync.remove(LEGACY_STORAGE_KEY);
+    } else if (items[STORAGE_KEY] != null) {
+      remoteStoredText = items[STORAGE_KEY];
+    }
+    // Value defaults to an empty string if there is no stored value
+    textAreaEl.value = remoteStoredText;
+  });
+
+  const throttledStorageUpdate = _.throttle(e => {
+    storageObject[STORAGE_KEY] = textAreaEl.value;
+    storage.sync.set(storageObject, function (result) { /* empty */
     });
+  }, CHANGE_DELAY);
 
-    const throttledStorageUpdate = _.throttle(e => {
-        storageObject[STORAGE_KEY] = textAreaEl.value;
-        storage.sync.set(storageObject, function(result) { /* empty */ });
-    }, CHANGE_DELAY);
+  const throttledUsageUpdate = _.throttle(updateUsage, 1000);
 
-    const throttledUsageUpdate = _.throttle(updateUsage, 1000);
-
-    // update storage which in turns fires the onChange event below
-    textAreaEl.addEventListener('keyup', throttledStorageUpdate);
-    textAreaEl.addEventListener('keyup', throttledUsageUpdate);
+  // update storage which in turns fires the onChange event below
+  textAreaEl.addEventListener('keyup', throttledStorageUpdate);
+  textAreaEl.addEventListener('keyup', throttledUsageUpdate);
 
 })(chrome);
